718900a7ab9831f45f8cfd984f0b9cb378599bf7
searchme86 <searchme86@gmail.com>
2022-12-02 16:55:23 +0900

    ✨ middleware functions

---
+++ src/middleware/logger.js
@@ -0,0 +1,7 @@
+export const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
