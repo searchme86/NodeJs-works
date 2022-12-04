b505b669d086740e6240ffebbb9964cf43232ef0
searchme86 <searchme86@gmail.com>
2022-12-04 19:10:50 +0900

    ✨ add try/catch in controller

--- src/controllers/TaskContainer.js
+++ src/controllers/TaskContainer.js
@@ -1,23 +1,40 @@
-export const getAllTask = (req, res) => {
-  res.send('all items from the file');
+import Task from '../models/TaskMangerModel.js';
+
+export const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    return res.status(201).json({ task });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
 };
 
-export const createTask = (req, res) => {
-  res.json(req.body);
+export const getAllTask = (req, res) => {
+  try {
+    return res.send('all items from the file');
+  } catch (error) {}
 };
 
 export const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+  try {
+    return res.json({ id: req.params.id });
+  } catch (error) {}
 };
 
 export const getSingleTask = (req, res) => {
-  res.send('get single task');
+  try {
+    return res.send('get single task');
+  } catch (error) {}
 };
 
 export const updateTask = (req, res) => {
-  res.send('update task');
+  try {
+    return res.send('update task');
+  } catch (error) {}
 };
 
 export const deleteTask = (req, res) => {
-  res.send('delete task');
+  try {
+    return res.send('delete task');
+  } catch (error) {}
 };
