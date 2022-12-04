d748955d26f28f2bc0d05160e6c3bfda95c0c99e
searchme86 <searchme86@gmail.com>
2022-12-04 22:23:34 +0900

    ✨ fix server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import TaskRouter from './routes/TaskRouter';
 import { notFound } from './middleware/notFound';
+import { ErrorHandler } from './middleware/errorHandler';
 
 const app = express();
 
 // middleware
-
 // apply static files
 app.use(express.static('./public'));
 //parse form data
@@ -15,6 +15,9 @@ app.use(express.json());
 
 // Router
 app.use('/api/v1/tasks', TaskRouter);
+
+// Error Handler
 app.use(notFound);
+app.use(ErrorHandler);
 
 export default app;
