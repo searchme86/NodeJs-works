2131b38fbb630d219ebf1faea43c30721bbe817e
searchme86 <searchme86@gmail.com>
2022-12-05 14:46:44 +0900

    👏🏻 store model completed

---
+++ src/models/Store.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'product name must be provided'],
+  },
+  price: {
+    type: Number,
+    require: [true, 'product price must be provided'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ['ikea', 'liddy', 'caressa', 'marcos'],
+      value: '{VALUE} is not supported',
+    },
+  },
+});
+
+const Store = mongoose.model('Store', productSchema);
+
+export default Store;
