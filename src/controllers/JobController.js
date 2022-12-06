fecb11c7b686961c6bcc3852ba350e0fd4fc8966
searchme86 <searchme86@gmail.com>
2022-12-07 00:01:50 +0900

    👏🏻 add stats controller

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -70,8 +70,48 @@ export const updateUser = async (req, res) => {
 };
 
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
-  res.status(StatusCode.OK).json({ jobs, count: jobs.length });
+  const { search, status, jobType, sort } = req.query;
+
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
+
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' };
+  }
+  if (status && status !== 'all') {
+    queryObject.status = status;
+  }
+  if (jobType && jobType !== 'all') {
+    queryObject.jobType = jobType;
+  }
+  let result = Job.find(queryObject);
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position');
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position');
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const jobs = await result;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCode.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 export const getJob = async (req, res) => {
@@ -87,7 +127,7 @@ export const getJob = async (req, res) => {
   if (!job) {
     throw new NotFoundError(`No job with id ${jobId}`);
   }
-  res.status(StatusCode.OK).json({ job });
+  res.status(StatusCodes.OK).json({ job });
 };
 
 export const createJobs = async (req, res) => {
@@ -106,6 +146,9 @@ export const updateJobs = async (req, res) => {
   if (company === '' || position === '') {
     throw new BadRequest('Company or Position fields cannot be empty');
   }
+
+  console.log('req.body', req.body);
+
   const job = await Job.findByIdAndUpdate(
     { _id: jobId, createdBy: userId },
     req.body,
@@ -131,4 +174,51 @@ export const deleteJobs = async (req, res) => {
     throw new NotFoundError(`No job with id ${jobId}`);
   }
   res.status(StatusCode.OK).send();
+};
+
+export const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: '$status', count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format('MMM Y');
+      return { date, count };
+    })
+    .reverse();
+
+  res.status(StatusCode.OK).json({ defaultStats, monthlyApplications });
 };
